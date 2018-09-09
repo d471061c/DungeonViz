a2806e66c772b7ebb9da5d061d4ce04ab33045ac
d471061c <cpastethat@gmail.com>
2018-09-09 20:05:49 +0300

    Add java project

---
+++ DungeonViz/src/main/java/com/d471061c/dungeonviz/DungeonViz.java
@@ -0,0 +1,19 @@
+package com.d471061c.dungeonviz;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author d471061c
+ */
+public class DungeonViz {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
