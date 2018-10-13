a5661388ecbe9937d3d7081406622d694ffbaca9
d471061c <cpastethat@gmail.com>
2018-10-13 10:52:59 +0300

    Remove debug code

--- DungeonViz/src/main/java/com/d471061c/dungeonviz/DungeonViz.java
+++ DungeonViz/src/main/java/com/d471061c/dungeonviz/DungeonViz.java
@@ -10,24 +10,6 @@ import java.util.Arrays;
  * @author d471061c
  */
 public class DungeonViz {
-
-
-    public static PrimObject createPrimObject(double value, int index) {
-        return new PrimObject(null, value, index);
-    }
-    
-    public static void displayArray(PrimObjectHeap heap) {
-        Comparable objects[] = heap.getArray();
-        System.out.print("< Array > ");
-        for (int i = 1; i <= heap.getHeapSize(); i++) {
-            if (objects[i] != null) {
-                System.out.print(((PrimObject)objects[i]).getValue() + " ");
-            }
-            
-        }
-        System.out.println();
-    }
-    
     
     /***
      * Generate and display the dungeon
