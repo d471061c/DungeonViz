8346c8dd3f77e43db08eb1ec1cbe87e113cd9c07
d471061c <cpastethat@gmail.com>
2018-10-17 08:04:00 +0300

    Add cli

--- DungeonViz/src/main/java/com/d471061c/dungeonviz/DungeonViz.java
+++ DungeonViz/src/main/java/com/d471061c/dungeonviz/DungeonViz.java
@@ -3,7 +3,10 @@ package com.d471061c.dungeonviz;
 import com.d471061c.dungeonviz.domain.Dungeon;
 import com.d471061c.dungeonviz.domain.PrimObject;
 import com.d471061c.dungeonviz.domain.datastructures.PrimObjectHeap;
+import com.d471061c.dungeonviz.exception.InvalidArgumentException;
 import com.d471061c.dungeonviz.logic.DungeonGenerator;
+import com.d471061c.dungeonviz.logic.RoomFactory;
+import com.d471061c.dungeonviz.utils.CommandLineParser;
 import java.util.Arrays;
 
 /**
@@ -11,19 +14,48 @@ import java.util.Arrays;
  */
 public class DungeonViz {
     
-    /***
-     * Generate and display the dungeon
-     * @param numberOfRooms Number of rooms to generate in the dungeon
-     */
-    public static void generateAndDisplayDungeon(int numberOfRooms) {
-        DungeonGenerator generator = new DungeonGenerator();
-        Dungeon dungeon = generator.generateDungeon(numberOfRooms);
-        System.out.println(dungeon);
+    public static void parseCommandLine(String[] args) {
+        
+        CommandLineParser parser = new CommandLineParser();
+        if (args.length == 0) {
+            parser.displayInfo();
+            return;
+        }
+        
+        try {
+            parser.parse(args);
+            
+            
+        } catch (InvalidArgumentException ex) {
+            System.out.println(ex);
+        }
+        
+        RoomFactory roomFactory = new RoomFactory();
+            
+            // Will be refactored in future
+            if (parser.getHeight() > 10) {
+                roomFactory.setMaximumHeight(parser.getHeight());
+            }
+            
+            if (parser.getWidth() > 10) {
+                roomFactory.setMaximumWidth(parser.getWidth());
+            }
+            
+            roomFactory.setFixedSize(parser.isFixedSize());
+            int rooms = parser.getRooms() > 0 ? parser.getRooms() : 5;
+            
+            //System.out.format("Generated dungeon with %d rooms.\n", rooms);
+            
+            DungeonGenerator generator = new DungeonGenerator();
+            Dungeon dungeon = generator.generateDungeon(rooms);
+            
+            
+            System.out.println(dungeon);
+        
     }
 
     public static void main(String[] args) {
-        int rooms = 3;
-        generateAndDisplayDungeon(rooms);
+        parseCommandLine(args);
     }
 
 }
